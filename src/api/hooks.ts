0bebcd1ed58d3c099619b2cff4edadc605e66ab5
Dusan Ilic <dusan.ilic1999@gmail.com>
2025-11-18 16:09:20 +0100

    fix: fix

--- src/api/hooks.ts
+++ src/api/hooks.ts
@@ -1,7 +1,7 @@
 import type { TDistributer, TSoil, TUser } from "src/types";
 
 export const registerHook = async (object: TUser) => {
-  const res = await fetch(`http://localhost:3003/api/admin`, {
+  const res = await fetch(`https://soilbackend.dusanprogram.eu/api/admin`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -17,7 +17,7 @@ export const loginHook = async (object: {
   username: string;
   password: string;
 }) => {
-  const res = await fetch(`http://localhost:3003/api/login`, {
+  const res = await fetch(`https://soilbackend.dusanprogram.eu/api/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -30,19 +30,19 @@ export const loginHook = async (object: {
 };
 
 export const fetchSoilDetails = async () => {
-  const res = await fetch(`http://localhost:3003/api/soil/details`);
+  const res = await fetch(`https://soilbackend.dusanprogram.eu/api/soil/details`);
   if (!res.ok) throw new Error("Failed to fetch soil details");
   return await res.json();
 };
 
 export const fetchDistributerDetails = async () => {
-  const res = await fetch(`http://localhost:3003/api/distributer/details`);
+  const res = await fetch(`https://soilbackend.dusanprogram.eu/api/distributer/details`);
   if (!res.ok) throw new Error("Failed to fetch distributer details");
   return await res.json();
 };
 
 export const addSoilDetail = async (object: TSoil) => {
-  const res = await fetch("http://localhost:3003/api/soil/details", {
+  const res = await fetch("https://soilbackend.dusanprogram.eu/api/soil/details", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(object),
@@ -53,7 +53,7 @@ export const addSoilDetail = async (object: TSoil) => {
 };
 
 export const addDistributerDetail = async (object: TDistributer) => {
-  const res = await fetch("http://localhost:3003/api/distributer/details", {
+  const res = await fetch("https://soilbackend.dusanprogram.eu/api/distributer/details", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(object),
